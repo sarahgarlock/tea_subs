9d5e27fde174c56a124d00d3fab3fe23126f7aac
Sarah Garlock <garlock.sarah10@gmail.com>
2023-09-11 17:31:16 -0600

    add subscription validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,9 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   has_and_belongs_to_many :teas
+
+  validates_presence_of :title
+  validates_presence_of :price
+  validates_presence_of :status
+  validates_presence_of :frequency
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Subscription, type: :model do
     it { should belong_to :customer }
     it { should have_and_belong_to_many :teas }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :price }
+    it { should validate_presence_of :status }
+    it { should validate_presence_of :frequency }
+  end
 end
