ae339ea4cf5734f45faf8270f5ff7162918ae879
Sarah Garlock <garlock.sarah10@gmail.com>
2023-09-11 17:31:24 -0600

    add tea validations

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,3 +1,8 @@
 class Tea < ApplicationRecord
   has_and_belongs_to_many :subscriptions
+
+  validates_presence_of :title
+  validates_presence_of :description
+  validates_presence_of :temperature
+  validates_presence_of :brew_time
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Tea, type: :model do
   describe 'relationships' do
     it { should have_and_belong_to_many :subscriptions }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :temperature }
+    it { should validate_presence_of :brew_time }
+  end
 end
