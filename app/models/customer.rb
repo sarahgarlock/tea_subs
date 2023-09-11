0be03dbfc38d8f429a9f7c148a25fc57b632cf12
Sarah Garlock <garlock.sarah10@gmail.com>
2023-09-11 17:27:45 -0600

    add customer validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,8 @@
 class Customer < ApplicationRecord
   has_many :subscriptions
+
+  validates_presence_of :first
+  validates_presence_of :last
+  validates_presence_of :email
+  validates_presence_of :address
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Customer, type: :model do
   describe 'relationships' do
     it { should have_many :subscriptions }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :first }
+    it { should validate_presence_of :last }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :address }
+  end
 end
