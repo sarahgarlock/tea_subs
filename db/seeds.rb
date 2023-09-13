09f421dd431b6b1a5347c3c0179c8857dfe479d8
Sarah Garlock <garlock.sarah10@gmail.com>
2023-09-13 14:23:37 -0600

    add more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,9 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 customer_1 = Customer.create(first: "Johny", last: "Flow", email: "jflow@gmail.com", address: "123 Fake St")
+customer_2 = Customer.create(first: "Raj", last: "Jamnis", email: "rjamnis@gmail.com", address: "321 Not Real St")
 
-subscription_1 = Subscription.create(title: "Earl Grey", price: "10.00", status: 0, frequency: "monthly", customer_id: customer_1.id)
+subscription_1 = Subscription.create(title: "Earl Grey", price: "10.00", status: 1, frequency: "monthly", customer_id: customer_1.id)
+subscription_2 = Subscription.create(title: "Green Tea", price: "4.00", status: 2, frequency: "monthly", customer_id: customer_1.id)
+subscription_3 = Subscription.create(title: "Black Tea", price: "7.00", status: 1, frequency: "monthly", customer_id: customer_2.id)
+subscription_4 = Subscription.create(title: "Green Tea", price: "3.00", status: 2, frequency: "monthly", customer_id: customer_2.id)
