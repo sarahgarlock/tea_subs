596cc5edc645df0660a06a948c0c5aa14684e055
Sarah Garlock <garlock.sarah10@gmail.com>
2023-09-13 14:24:55 -0600

    add raise error for not found

--- spec/requests/api/v1/delete_sub_request.rb
+++ spec/requests/api/v1/delete_sub_request.rb
@@ -4,12 +4,13 @@ RSpec.describe 'Cancel a subscription' do
   it 'can cancel a subscription' do
     customer = create(:customer)
     subscription = create(:subscription, customer_id: customer.id)
-
+    
     expect(Subscription.count).to eq(1)
 
     delete "/api/v1/customers/#{customer.id}/subscriptions/#{subscription.id}"
 
     expect(response.status).to eq(204)
     expect(Subscription.count).to eq(0)
+    expect{ Subscription.find(subscription.id) }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
